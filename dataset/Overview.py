06d4344984fef624c1732e8f178a101863bea9a0
HappyCat-9487 <lianglu3366@gmail.com>
2024-06-05 11:36:44 +0200

    Update

--- dataset/Overview.py
+++ dataset/Overview.py
@@ -19,8 +19,9 @@ file_paths = [
     './Imagination/e_close/trial_0.csv',
 ]
 '''
-
-file_paths = glob.glob(base_dir + '/**/*.csv', recursive=True)
+#print(base_dir)
+#file_paths = glob.glob(base_dir + '/**/*.csv', recursive=True)
+file_paths = glob.glob(base_dir + '/without_individuals/*.csv', recursive=True)
 
 for file_path in file_paths:
     df = pd.read_csv(file_path)
@@ -31,7 +32,7 @@ for file_path in file_paths:
 
     # Check if it don't have any vacancy in the data field
     if df.isnull().values.any():
-        print("There are some NaN in the data.")
+        print("Ohh no!! There are some NaN in the data.")
     else:
         print("There are no NaN in the data.")
     print()
@@ -64,3 +65,31 @@ for file_path in file_paths:
     if any("TF9" in col for col in df.columns):
         df.rename(columns=lambda x: x.replace('TF9', 'TP9'), inplace=True)
         df.to_csv(file_path, index=False)
+        
+#%%
+#Combine the data in the CSV file with the same categories without differentiating the individual trials
+#and save it to the new CSV file in the without_individual folder
+def merge(csv_files):
+    df_list = [pd.read_csv(file) for file in csv_files]
+    merged_df = pd.concat(df_list).sort_values('Image')
+    return merged_df
+
+csv_files = glob.glob('./Pictures/e_close/motion/trial_*.csv')
+merged_df = merge(csv_files)
+merged_df.to_csv('./without_individuals/e_close/pic_e_close_motion.csv', index=False)
+
+csv_files = glob.glob('./Pictures/e_close/noun/trial_*.csv')
+merged_df = merge(csv_files)
+merged_df.to_csv('./without_individuals/e_close/pic_e_close_noun.csv', index=False)
+
+csv_files = glob.glob('./Pictures/e_open/motion/trial_*.csv')
+merged_df = merge(csv_files)
+merged_df.to_csv('./without_individuals/e_open/pic_e_open_motion.csv', index=False)
+
+csv_files = glob.glob('./Pictures/e_open/noun/trial_*.csv')
+merged_df = merge(csv_files)
+merged_df.to_csv('./without_individuals/e_open/pic_e_open_noun.csv', index=False)
+
+csv_files = glob.glob('./Imagination/e_close/trial_*.csv')
+merged_df = merge(csv_files)
+merged_df.to_csv('./without_individuals/imagination.csv', index=False)
